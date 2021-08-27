14fa5cdce27f003e5ed3cc79c9fe9fb7b39022b0
LeoCosta-Dev <leonardodacostadasilva@hotmail.com>
2021-08-27 00:34:35 -0300

    cifra de cesar no js

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
 /*.charCodeAt() - transforma letra em número da tabela ascii
 String.fromCharCode(b) - faz o inverso do de cima */
+var mensagem = "eu amo paçoca"
+var incremento = 2
+
+function cifrado(){
+    var mensagemArr = mensagem.split('')
+    var mensagemCifrada = []
+    var codificadorArr = []
+    
+    for (var i = 0; mensagemArr.length > i; i++){
+        if (mensagemArr[i] !== ' '){
+            codificadorArr.push((mensagemArr[i].charCodeAt()) + incremento)
+        }else{
+            codificadorArr.push(mensagemArr[i].charCodeAt())
+        }
+    }
+    for (var j = 0; codificadorArr.length > j; j++){
+        mensagemCifrada.push(String.fromCharCode(codificadorArr[j]))
+    }
+    return mensagemCifrada.join('')
+
+}
+function decifrado(){
+    var mensagemArr = mensagem.split('')
+    var mensagemCifrada = []
+    var codificadorArr = []
+    
+    for (var i = 0; mensagemArr.length > i; i++){
+        if (mensagemArr[i] !== ' '){
+            codificadorArr.push((mensagemArr[i].charCodeAt()) - incremento)
+        }else{
+            codificadorArr.push(mensagemArr[i].charCodeAt())
+        }
+    }
+    for (var j = 0; codificadorArr.length > j; j++){
+        mensagemCifrada.push(String.fromCharCode(codificadorArr[j]))
+    }
+    return mensagemCifrada.join('')
+
+}
