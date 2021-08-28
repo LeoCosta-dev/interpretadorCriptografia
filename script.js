2b8fe27ca05419383f62420787a1418571512944
LeoCosta-Dev <leonardodacostadasilva@hotmail.com>
2021-08-28 20:57:36 -0300

    remoção de excessos de parenteses

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ function cifrado(){
     
     for (let i = 0; mensagemArr.length > i; i++){
         if (mensagemArr[i].charCodeAt() >= 65 && mensagemArr[i].charCodeAt() <=90 ){
-            let teste = (((mensagemArr[i].charCodeAt()) -65 + incremento)%26)
+            let teste = ((mensagemArr[i].charCodeAt()) -65 + incremento)%26
             codificadorArr.push(teste+65)
             console.log("cheguei")
         }else if(mensagemArr[i].charCodeAt() >= 97 && mensagemArr[i].charCodeAt() <=122){
-            let teste = (((mensagemArr[i].charCodeAt()) -97 + incremento)%26)
+            let teste = ((mensagemArr[i].charCodeAt()) -97 + incremento)%26
             codificadorArr.push(teste+97)
             console.log("cheguei")
         }else{
@@ -40,11 +40,11 @@ function decifrado(){
     
     for (let i = 0; mensagemArr.length > i; i++){
         if (mensagemArr[i].charCodeAt() >= 65 && mensagemArr[i].charCodeAt() <=90 ){
-            let teste = (((mensagemArr[i].charCodeAt()) -65 - incremento)%26)
+            let teste = ((mensagemArr[i].charCodeAt()) -65 - incremento)%26
             codificadorArr.push((teste < 0?teste+26:teste)+65)
             console.log("cheguei")
         }else if(mensagemArr[i].charCodeAt() >= 97 && mensagemArr[i].charCodeAt() <=122){
-            let teste = (((mensagemArr[i].charCodeAt()) -97 - incremento)%26)
+            let teste = ((mensagemArr[i].charCodeAt()) -97 - incremento)%26
             codificadorArr.push((teste < 0?teste+26:teste)+97)
             console.log("cheguei")
         }else{
